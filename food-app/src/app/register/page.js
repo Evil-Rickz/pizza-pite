2d574f00b7e5ce6a48e29e2bb199beebea9f694a
Rick <94098966+Evil-Rickz@users.noreply.github.com>
2024-05-07 14:58:45 +0000

    starting authentication

--- food-app/src/app/register/page.js
+++ food-app/src/app/register/page.js
@@ -2,14 +2,22 @@ import Facebook from "@/components/icons/Facebook";
 import Google from "@/components/icons/Google";
 
 export default function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    function handleFormSubmit(ev) {
+        ev.preventDefault();
+    }
+
     return (
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl mb-8">
                 Register
             </h1>
-            <form className="block max-w-xs mx-auto">
-                <input type="email" placeholder="email"/>
-                <input type="password" placeholder="password"/>
+            <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input type="email" placeholder="email" value={email}
+                    onChange={ev => setEmail(ev.target.value)} />
+                <input type="password" placeholder="password" value={password}
+                    onChange={ev => setPassword(ev.target.value)} />
                 <button type="submit">Register</button>
                 <div className="my-8 text-center text-gray-400">
                     or login with provider
